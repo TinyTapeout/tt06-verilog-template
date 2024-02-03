0fb52eccb53739a6c6d4476da5af1f45e5b6752e
matt venn <matt@mattvenn.net>
2024-02-03 18:37:32 +0100

    Update project.v
    comments for ena, clk and rst_n

--- src/project.v
+++ src/project.v
@@ -11,9 +11,9 @@ module tt_um_example (
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,
-    input  wire       clk,
-    input  wire       rst_n
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
   // All output pins must be assigned. If not used, assign to 0.
